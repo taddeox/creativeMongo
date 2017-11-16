bfa3c2b6d9b4ce662632261bfdafa170c9ae378f
taddeox <taddeox@gmail.com>
2017-11-16 16:11:40 +0000

    finished basic front end, added pics

--- vote/public/javascripts/vote.js
+++ vote/public/javascripts/vote.js
@@ -4,10 +4,30 @@ var questions = [
     "Android", 
     "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201709101434", 
     "Apple"],
-    ["",
-    "",
-    "",
-    ""]
+    ["https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Toilet_paper_orientation_over.jpg/200px-Toilet_paper_orientation_over.jpg",//toilet paper
+    "Over",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Toilet_paper_orientation_under.jpg/200px-Toilet_paper_orientation_under.jpg",
+    "Under"],
+    ["http://3.bp.blogspot.com/-BtDZmie7xQw/UXlqDpYCAUI/AAAAAAAAA4w/Rlf8CE11ZlE/s1600/same_line.jpg",//Bracket
+    "Inline",
+    "https://2.bp.blogspot.com/-8g2_JXniueg/UXlqDw5g8uI/AAAAAAAAA5A/v54Vhbl8Lcw/s1600/separate_line.jpg",
+    "Newline"],
+    ["end.PNG",//tooth paste
+    "End",
+    "middle.PNG",
+    "Middle"],
+    ["https://winblogs.azureedge.net/win/2016/12/cropped-cropped-Windows-logo1-270x270.png",
+    "PC",
+    "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201709101434",
+    "Mac"],
+    ["https://www.facebook.com/images/fb_icon_325x325.png",
+    "Facebook",
+    "https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=w300",
+    "Instagram"],
+    ["camelCase.PNG",
+    "CamelCase",
+    "underscore.PNG",
+    "Underscore"],
 ];
 
 $(document).ready(function(){
@@ -24,8 +44,16 @@ $(document).ready(function(){
     console.log(userSide);
     console.log(currentQuestion);
 
-    /*
-    if(0 < questions.length){
+    var myobj = {Id:currentQuestion,Side:userSide};
+    currentQuestion++;
+    jobj = JSON.stringify(myobj);
+    $("#json").text(jobj);
+
+
+
+
+    
+    if(currentQuestion < questions.length){
         $("#pic1").attr("src", questions[currentQuestion][0]);
         $("#des1").html(questions[currentQuestion][1]);
         $("#pic2").attr("src", questions[currentQuestion][2]);
@@ -33,15 +61,19 @@ $(document).ready(function(){
         //todo:finish this/ make it work
     }
     else{
+        $("#header").html("Results");
+        $("#boxa").html("a");
+        $("#boxb").html("b");
+        $("#boxc").html("c");
+        $("#boxd").html("d");
+        $("#boxe").html("e");
+        $("#boxf").html("f");
        //todo:send to results page
     }
-    */
+    
 
 
-    var myobj = {Id:currentQuestion,Side:userSide};
-    currentQuestion++;
-    jobj = JSON.stringify(myobj);
-    $("#json").text(jobj);
+
 
     var url = "vote";
     $.ajax({
